33016b9e5994c2764577303bb540c8a63e190312
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-02-27 16:03:07 +0100

    Fixed problem w/ cshtml creation. Error: wrong folders. Fixed by directly rightclicking on controller ActionResult

--- U2S2D2/Controllers/DipendentiController.cs
+++ U2S2D2/Controllers/DipendentiController.cs
@@ -10,12 +10,18 @@ using U2S2D2.Models;
 namespace U2S2D2.Controllers
 {
     public class DipendentiController : Controller
-    {
-        // GET: Dipendenti
+    {   
+        //CONTROLLER DIPENDENTI
+        
+
+        //apro connessione nell'action result Index
         public ActionResult Index()
         {
             string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
             SqlConnection connection = new SqlConnection(connectionString);
+
+            //creo una lista di tipo Dipendente chiamata dipendenti
+            List<Dipendente> dipendenti = new List<Dipendente>();
 
             try
             {
@@ -23,7 +29,25 @@ namespace U2S2D2.Controllers
                 string query = "SELECT * FROM Dipendenti";
                 SqlCommand cmd = new SqlCommand(query, connection);
                 SqlDataReader reader = cmd.ExecuteReader();
-                List<Dipendente> dipendenti = new List<Dipendente>();
+
+                // creo ciclo while per leggere i dati dal database e inserirli in una lista di oggetti Dipendente
+                while (reader.Read())
+                {
+                    //creo un oggetto Dipendente chiamato dipendente
+                    Dipendente dipendente = new Dipendente
+                    {
+                        ID = Convert.ToInt32(reader["ID"]),
+                        Nome = reader["Nome"].ToString(),
+                        Cognome = reader["Cognome"].ToString(),
+                        Indirizzo = reader["Indirizzo"].ToString(),
+                        CodiceFiscale = reader["CodiceFiscale"].ToString(),
+                        Coniugato = Convert.ToBoolean(reader["Coniugato"]),
+                        NumeroFigli = Convert.ToInt32(reader["NumeroFigli"]),
+                        Mansione = reader["Mansione"].ToString()
+                    };
+                    //aggiungo l'oggetto Dipendente alla lista
+                    dipendenti.Add(dipendente);
+                }
             }
             catch (Exception ex)
             {
@@ -33,9 +57,54 @@ namespace U2S2D2.Controllers
             {
                 connection.Close();
             }
+
+            return View(dipendenti);
+        }
+
+        
+        public ActionResult CreaDipendenti()
         {
             return View();
         }
+
+        //uso httpost per inviare i dati al database
+        [HttpPost]
+
+        //action result per la creazione di un nuovo dipendente con connessione al database e query di inserimento dei dati nel database 
+        public ActionResult CreaDipendenti(Dipendente dipendente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                //query per inserire i dati nel database tramite i parametri passati dal form di creazione di un nuovo dipendente
+                string query = "INSERT INTO Dipendenti (Nome, Cognome, Indirizzo, CodiceFiscale, Coniugato, NumeroFigli, Mansione) VALUES (@Nome, @Cognome, @Indirizzo, @CodiceFiscale, @Coniugato, @NumeroFigli, @Mansione)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Nome", dipendente.Nome);
+                cmd.Parameters.AddWithValue("@Cognome", dipendente.Cognome);
+                cmd.Parameters.AddWithValue("@Indirizzo", dipendente.Indirizzo);
+                cmd.Parameters.AddWithValue("@CodiceFiscale", dipendente.CodiceFiscale);
+                cmd.Parameters.AddWithValue("@Coniugato", dipendente.Coniugato);
+                cmd.Parameters.AddWithValue("@NumeroFigli", dipendente.NumeroFigli);
+                cmd.Parameters.AddWithValue("@Mansione", dipendente.Mansione);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return RedirectToAction("Index");
         }
+
+  
+
     }
-}
+    }
+
--- U2S2D2/Controllers/PagamentiController.cs
+++ U2S2D2/Controllers/PagamentiController.cs
@@ -1,17 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using U2S2D2.Models;
 
 namespace U2S2D2.Controllers
-{
+{   
+    //CONTROLLER PAGAMENTI
     public class PagamentiController : Controller
     {
-        // GET: Pagamenti
-        public ActionResult Index()
+      //apro connessione nell'action result index
+      
+      public ActionResult Index()
+        {  
+           string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+           SqlConnection connection = new SqlConnection(connectionString);
+
+            //creo una lista di tipo Pagamento chiamata pagamenti
+            List<Pagamento> pagamenti = new List<Pagamento>();
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Pagamenti";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                //creo ciclo while per leggere i dati dal database e inserirli in una lista di oggetti Pagamento
+                while (reader.Read())
+                {
+                    //creo un oggetto Dipendente chiamato dipendente
+                    Pagamento pagamento = new Pagamento
+                    {
+                        ID = Convert.ToInt32(reader["ID"]),
+                        IDDipendente = Convert.ToInt32(reader["IDDipendente"]),
+                        PeriodoPagamento = reader["PeriodoPagamento"].ToString(),
+                        AmmontarePagamento = Convert.ToDecimal(reader["AmmontarePagamento"]),
+                        TipoPagamento = reader["TipoPagamento"].ToString()
+
+                    };
+                    //aggiungo l'oggetto Dipendente alla lista
+                    pagamenti.Add(pagamento);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return View(pagamenti);
+        }
+
+        public ActionResult CreaPagamenti()
         {
             return View();
         }
+
+        //uso httpost per inviare i dati al database
+        [HttpPost]
+        public ActionResult CreaPagamenti(Pagamento pagamento)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                //query per inserire i dati nel database tramite i parametri passati dal form di creazione di un nuovo pagamento 
+                string query = "INSERT INTO Pagamenti (IDDipendente, PeriodoPagamento, AmmontarePagamento, TipoPagamento) VALUES (@IDDipendente, @PeriodoPagamento, @AmmontarePagamento, @TipoPagamento)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@IDDipendente", pagamento.IDDipendente);
+                cmd.Parameters.AddWithValue("@PeriodoPagamento", pagamento.PeriodoPagamento);
+                cmd.Parameters.AddWithValue("@AmmontarePagamento", pagamento.AmmontarePagamento);
+                cmd.Parameters.AddWithValue("@TipoPagamento", pagamento.TipoPagamento);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //ritorno alla view Index dopo aver inserito i dati nel database e visualizzo la lista aggiornata
+            return RedirectToAction("Index");
+        }
+
     }
 }
