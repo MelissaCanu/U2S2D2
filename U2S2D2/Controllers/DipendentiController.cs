3320198db621fca766dd9bdf7872552054d7b8cc
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-02-27 19:43:08 +0100

    EditDipendenti get and post with update query added

--- U2S2D2/Controllers/DipendentiController.cs
+++ U2S2D2/Controllers/DipendentiController.cs
@@ -103,6 +103,81 @@ namespace U2S2D2.Controllers
             return RedirectToAction("Index");
         }
 
+        //get action result per la modifica di un dipendente con form precompilato con i dati del dipendente selezionato
+        public ActionResult EditDipendenti(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            Dipendente dipendente = new Dipendente();
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Dipendenti WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@ID", id);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dipendente.ID = Convert.ToInt32(reader["ID"]);
+                    dipendente.Nome = reader["Nome"].ToString();
+                    dipendente.Cognome = reader["Cognome"].ToString();
+                    dipendente.Indirizzo = reader["Indirizzo"].ToString();
+                    dipendente.CodiceFiscale = reader["CodiceFiscale"].ToString();
+                    dipendente.Coniugato = Convert.ToBoolean(reader["Coniugato"]);
+                    dipendente.NumeroFigli = Convert.ToInt32(reader["NumeroFigli"]);
+                    dipendente.Mansione = reader["Mansione"].ToString();
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return View(dipendente);
+        }
+
+        //HttpPost action result per la modifica di un dipendente con connessione al database e query di update dei dati nel database
+        [HttpPost]
+        public ActionResult EditDipendenti(Dipendente dipendente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                string query = "UPDATE Dipendenti SET Nome = @Nome, Cognome = @Cognome, Indirizzo = @Indirizzo, CodiceFiscale = @CodiceFiscale, Coniugato = @Coniugato, NumeroFigli = @NumeroFigli, Mansione = @Mansione WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@ID", dipendente.ID);
+                cmd.Parameters.AddWithValue("@Nome", dipendente.Nome);
+                cmd.Parameters.AddWithValue("@Cognome", dipendente.Cognome);
+                cmd.Parameters.AddWithValue("@Indirizzo", dipendente.Indirizzo);
+                cmd.Parameters.AddWithValue("@CodiceFiscale", dipendente.CodiceFiscale);
+                cmd.Parameters.AddWithValue("@Coniugato", dipendente.Coniugato);
+                cmd.Parameters.AddWithValue("@NumeroFigli", dipendente.NumeroFigli);
+                cmd.Parameters.AddWithValue("@Mansione", dipendente.Mansione);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return RedirectToAction("Index");
+        }
+
   
 
     }
